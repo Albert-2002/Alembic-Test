11829ed40e0fa8260e30b060bc93b06923d3973c
Albert G <albertag2002@gmail.com>
2025-03-04 23:39:23 +0530

    Refactor Alembic migrations and models: set target metadata, remove initial migration, and add autogenerated migration for students table.

--- app/models.py
+++ app/models.py
@@ -2,9 +2,13 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+metadata = Base.metadata
 
 class Student(Base):
     __tablename__ = "students"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
+
+    def __repr__(self):
+        return f"<Student(name={self.name})>"
